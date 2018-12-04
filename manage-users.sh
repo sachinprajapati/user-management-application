fa6c9c0a61c5ed76a1d4ba3290477af4d3550763
mikkujain <tavarsachin@gmail.com>
2018-12-04 17:33:43 +0530

    delete user

--- manage-users.sh
+++ manage-users.sh
@@ -1,4 +1,9 @@
 # !/bin/bash
+UserExist()
+{
+   awk -F":" '{ print $1 }' /etc/passwd | grep -x $1 > /dev/null
+   return $?
+}
 while true;
 do
 	printf "\n---------------------------------------------------------\nWelcome to the User Management Application for $USER\n1. Create a new user.\n2. Delete a user.\n3. Modify a user.\n4. See last 10 created users.\n5. Quit\n   Choice: "
@@ -12,16 +17,29 @@ NC='\033[0m' # No Color
 	  	if [[ $EUID -ne 0 ]]; then
 	  		printf "${RED}you don't have permission to create user${NC}\n"
 	  	else
-		  	printf 'please enter username:\t'
+		  	printf 'Username :\t'
 		  	read un
-			if getent passwd $un > /dev/null 2>&1; then
+		  	UserExist $un
+			if [ $? = 0 ] ; then
 
 				printf "${RED}Warning: This user already exists${NC}\nplease enter to continue............"
 				read
 			else
-				printf "Full Name: "
-				read fn
-				echo "you fn is $fn"
+				printf "Full Name :\t"
+				read fn;
+				printf "Home Directory Leave blank for default :\t"
+				read hd;
+				printf "Select Following Available Shell or Leave blank for default : \n"
+				tail -n +2 /etc/shells
+				printf "Enter Shell :\t"
+				read sl;
+				printf "Password:\t"
+				read -s ps;
+				printf "\n"
+				useradd $un
+				if [ -z "$ps" ]; then
+					echo -e "$ps\n$ps" | sudo passwd $un
+				fi
 			fi
 		fi
 	  ;; 
@@ -29,7 +47,21 @@ NC='\033[0m' # No Color
 	  if [[ $EUID -ne 0 ]]; then
 	  		printf "${RED}you don't have permission to delete user${NC}\n"
 	  	else
-		  	echo "you selected 2"
+		  	printf "Enter username:\t"
+		  	read un;
+		  	UserExist $un
+		  	if [ $? = 0 ] ; then
+				read -p "What is your first name? " ans
+				if [ [ $ans =~ ^[Yy]$ ] && [ -d "/home/$un" ] ]
+				then
+					userdel -r $un
+					echo "delete with dir"
+				else
+					userdel $un
+				fi
+			else
+				printf "${RED}Warning: user $un does not exist${NC}"
+			fi
 		fi
 	  ;; 
 	  3)
@@ -40,7 +72,8 @@ NC='\033[0m' # No Color
 		fi
 	  ;; 
 	  4)
-	  	w -h | cut -d' ' -f1 | sort | uniq
+	  	printf "Last 10 Created Users:\n"
+	  	awk -F'[/:]' '{if ($3 >= 1000 && $3 != 65534) print $1}' /etc/passwd
 	  ;;
 	  5)res='quit'
 	  	exit 1
